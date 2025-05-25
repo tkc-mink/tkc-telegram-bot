e6e1f74220eca7227313d9c538ceba8e303b8dd4
tkc-mink <tkc.mink@gmail.com>
2025-05-25 19:46:43 +0700

    Add files via upload

---
+++ tokens_updated.py
@@ -0,0 +1,6 @@
+BOT_TOKENS = {
+    "shiba_tkc": "7774250343:AAHuDwy-nd1obytZL1C-2PHsDAVZDs7WV4I",
+    "giantjiew": "7660238907:AAE5mAE0XZqcWdEn2kMFlTR8un94d4oRLwk",
+    "p_tyretkc": "8029492335:AAERvbsWOojRul0y1u1ZYwjFdyT7smp5U8E",
+    "speedtkc": "7426078648:AAEsTxPG37VqO8m61YeC3OVOfLvVvypwQw8",
+}
