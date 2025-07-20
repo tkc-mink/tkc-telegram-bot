c33ad25f8aa18bc10acc2737c5db904d9034b16f
TKC - Assistant <tkc.mink@gmail.com>
2025-07-20 22:57:44 +0700

    Delete set_webhook.py

--- set_webhook.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-TOKEN = '8113563670:AAEzvfUChu4TeRsSTtuTouc6IPfXWs4FnCk'  # <- ของจริง (คุณใส่มาแล้ว ใช้ได้เลย)
-WEBHOOK_URL = 'https://tkc-telegram-bot.onrender.com/webhook'  # <- ไม่มีจุดต่อท้าย
-
-url = f'https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}'
-
-response = requests.get(url)
-print(response.json())
