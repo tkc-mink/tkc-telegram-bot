0055b527fcaa783df7bba71d1ed1dd2eb6cb4e9d
TKC - Assistant <tkc.mink@gmail.com>
2025-07-20 17:20:12 +0700

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,6 +1,6 @@
 import requests
 
-TOKEN = '5111356360:AAEwTuchAe5RTsTuoc6zjFPxk5apnck'  # <- ของจริง (คุณใส่มาแล้ว ใช้ได้เลย)
+TOKEN = '8113563670:AAEzvfUChu4TeRsSTtuTouc6IPfXWs4FnCk'  # <- ของจริง (คุณใส่มาแล้ว ใช้ได้เลย)
 WEBHOOK_URL = 'https://tkc-telegram-bot.onrender.com/webhook'  # <- ไม่มีจุดต่อท้าย
 
 url = f'https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}'
