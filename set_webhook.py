76c3d9144e7cf40646ef5f8f558fb03707521ce9
TKC - Assistant <tkc.mink@gmail.com>
2025-07-20 16:42:49 +0700

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+TOKEN = "8113563670:AAEzvfUChu4TeRsSTtuTouc6IPfXWs4FnCk"
+WEBHOOK_URL = "https://tkc-telegram-bot.onrender.com/webhook"
 
 if not TOKEN or not WEBHOOK_URL:
     raise Exception("Missing BOT_TOKEN or WEBHOOK_URL in environment variables")
