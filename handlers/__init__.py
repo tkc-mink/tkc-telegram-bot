6eefcb7b7b6ffa688f205d9d27088a52f479abaf
TKC - Assistant <tkc.mink@gmail.com>
2025-07-23 18:54:46 +0700

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+from .history import *  # และไฟล์อื่นๆที่คุณมี เช่น review, weather ฯลฯ
+from .doc import *
+from .image import *
+# ...
+from .main_handler import handle_message  # <-- ให้คุณย้าย/รวม handle_message ไว้ที่ไฟล์นี้ แล้ว export
