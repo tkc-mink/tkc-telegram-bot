11b923f277d1de9783b25bd52499ea0021a97420
TKC - Assistant <tkc.mink@gmail.com>
2025-07-23 22:17:16 +0700

    Update gold.py

--- handlers/gold.py
+++ handlers/gold.py
@@ -1,6 +1,6 @@
 # handlers/gold.py
+from utils.gold_utils import get_gold_price
 from utils.message_utils import send_message
-from gold_utils import get_gold_price
 
-def handle_gold(chat_id: int, user_text: str):
+def handle_gold(chat_id, user_text):
     send_message(chat_id, get_gold_price())
